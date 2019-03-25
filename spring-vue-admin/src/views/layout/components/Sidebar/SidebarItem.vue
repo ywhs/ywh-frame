79b565c4cc3c238f3813a1e2ccdc190ae374e98b
ywh <1459471053@qq.com>
2019-03-25 17:43:18 +0800

    解决v-for和v-if不能混用，使用computed计算属性计算v-if

--- spring-vue-admin/src/views/layout/components/Sidebar/SidebarItem.vue
+++ spring-vue-admin/src/views/layout/components/Sidebar/SidebarItem.vue
@@ -14,7 +14,7 @@
         <span v-if="item.meta&&item.meta.title" slot="title">{{ item.meta.title }}</span>
       </template>
 
-      <template v-for="child in item.children" v-if="!child.hidden">
+      <template v-for="child in visibleChildren">
         <sidebar-item
           v-if="child.children&&child.children.length>0"
           :is-nest="true"
@@ -58,6 +58,12 @@ export default {
   data() {
     return {
       onlyOneChild: null
+    }
+  },
+  // 这个是解决上面v-if和v-for不能混合使用的方法，之前是v-for(child in item.child) v-if(!item.hidden),需要把一方放在计算属性中，而v-for比v-if的优先级高，所以把v-if放下来
+  computed: {
+    visibleChildren() {
+      return this.item.children.filter(child => !child.hidden)
     }
   },
   methods: {
