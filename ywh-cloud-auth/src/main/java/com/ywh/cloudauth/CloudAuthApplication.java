0b5a42a0ae6507258bc15d331397d9d9ab423c1b
ywh <1459471053@qq.com>
2019-06-23 16:50:54 +0800

    完善项目结构

---
+++ ywh-cloud-auth/src/main/java/com/ywh/cloudauth/CloudAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ywh.cloudauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAuthApplication.class, args);
+    }
+
+}
---
+++ ywh-cloud-gateway/src/main/java/com/ywh/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ywh.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+
+}
